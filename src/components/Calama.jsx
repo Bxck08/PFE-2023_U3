f4e6b858340c5a9b3f66c18284fd7420abbe3e09
GIAN CARLOS TABILO ARAYA <gian.tabilo@inacapmail.cl>
2023-07-10 18:20:33 -0400

    Añadir rutas y componentes

---
+++ src/components/Calama.jsx
@@ -0,0 +1,22 @@
+/* react core */
+
+
+import React, { useState, useEffect } from "react";
+
+
+
+/* View */
+const Calama = () => {
+
+    return (
+        <>
+            <h1>Calama </h1>
+            <p>Ciudad heorica bastion de chilenidad</p>
+            
+        </>
+    )
+
+}
+
+
+export default Calama
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,22 @@
+/* react core */
+
+
+import React, { useState, useEffect } from "react";
+
+//function HolaMundo(){}
+
+/* View */
+const HolaMundo = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            {/*<p className="algo">Diga algo</p>*/}
+
+        </>
+    )
+
+}
+
+
+export default HolaMundo
